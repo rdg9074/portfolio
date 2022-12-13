587fbda5df1e92e51e8fbabc830ef6da48baaddb
koyohan <hphk.rdg9074@gmail.com>
2022-12-13 19:45:22 +0900

    feat: app create

---
+++ fe/portfolio/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import RootRouter from "./routers/RootRouter";
+
+function App() {
+  return <RootRouter />;
+}
+
+export default App;
---
+++ fe/portfolio/src/routers/RootRouter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RootRouter() {
+  return <div></div>;
+}
+
+export default RootRouter;
