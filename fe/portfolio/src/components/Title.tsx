8395491fb4f0ef900a3e0a635eeeca83f2b7e2ed
rdg9074 <rdg9074@gmail.com>
2023-01-12 17:19:22 +0900

    0112

--- fe/portfolio/src/components/Title.tsx
+++ fe/portfolio/src/components/Title.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import "./Title.scss"
+import React from "react";
+import "./Title.scss";
 
 function Title() {
-    return (
-        <div id='title'>
-            <div className='title-name'>
-                <p>주니어 개발자 고요한입니다.</p>
-            </div>
-            <div className='title-intro'>
-                <p>비전공자이지만 지속적인 학습과 경험을 통해서 천</p>
-            </div>
-        </div>
-    );
+  return (
+    <div id="title">
+      <div className="title-name">
+        <p></p>
+      </div>
+      <div className="title-intro">
+        <p></p>
+      </div>
+    </div>
+  );
 }
 
 export default Title;
